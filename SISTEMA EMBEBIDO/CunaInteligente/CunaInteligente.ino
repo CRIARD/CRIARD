46d31c53d603c85944e9eaf7f51b43392e66fa80
Matias <mati.adagio@gmail.com>
2018-10-06 13:09:38 -0300

    Configuracion LDR
    Se configuro el sensor ldr con atenuacion, en conjunto con un led

--- SISTEMA EMBEBIDO/CunaInteligente/CunaInteligente.ino
+++ SISTEMA EMBEBIDO/CunaInteligente/CunaInteligente.ino
@@ -1,6 +1,9 @@
 //Fuente https://programarfacil.com/tutoriales/fragmentos/servomotor-con-arduino/
 #include <Servo.h>
+#define ESPERA_LECTURAS 2000 // tiempo en milisegundos entre lecturas de la intensidad de la luz
+
 //Variables del Servo
+
 #define PinServo      9   //Pin donde está conectado el servo 
 #define ServoCerrado  0   // posición inicial 
 #define ServoAbierto  180  // posición de 0 grados
@@ -12,31 +15,74 @@ int posicionServo = ServoCerrado; //Va a contener la ubicación del servo
 Servo servoMotor;
 
 //Variables del micrófono
-#define pinMicro      A5   //Pin donde está conectado el servo 
+#define pinMicro      A5   //Pin donde está conectado el microfono
 #define umbralRuido   72  //Valor que lee el microfono en silencio
 #define standby 20000
 int valorLlanto = 0;      //variable to store the value coming from the sensor
 int tiempoUltimoLlanto = 0;
 int tiempoSilencio = 0;
+int tiempoInicioProm = 0; // para calcular el ruido ambiente
+int tiempoFinProm = 0;
+int muestras = 0;
+//Variables LDR
+#define PinLDR A0 // Pin donde esta conectado el LDR
+#define PinLED 11 // Pin donde esta conectado el LED (PWM)
+long cronometro_lecturas=0;
+long tiempo_transcurrido;
+unsigned int luminosidad;
+float coeficiente_porcentaje=100.0/1023.0; // El valor de la entrada analógica va de 0 a 1023 y se quiere convertir a porcentaje que va de cero a 100
 
 
 void setup() {
   Serial.begin (9600);
   servoMotor.attach(PinServo); // el servo trabajará desde el pin definido como PinServo
   servoMotor.write(ServoCerrado);   // Desplazamos a la posición 0
+
+  pinMode(PinLDR,INPUT); //Defino el tipo de pin para el LDR (Entrada)
+  pinMode(PinLED,OUTPUT); //Defino el tipo de pin para el LED (Salida)
+  
   tiempoInicialAmaque = millis();
   tiempoSilencio = millis();
   tiempoUltimoLlanto = millis();
+  tiempoInicioProm = millis();
+  tiempoFinProm = 0;
 }
 
 void loop() {
-  amacarCuna();
-  escucharLlanto();
+  
+  //amacarCuna();
+  //escucharLlanto();
+  
+  //float luz= detectarLuz();//detecto la luz ambiente
+  //encenderLEDGradual(luz); //Enciendo el led gradualmente segun la luz ambiente.
 
-
+  
+  //ruidoAmbiente();
+  
+  
 }
 
+void encenderLEDGradual(float luz){
+  
+    analogWrite(PinLED,luz);   
+  }
 
+float detectarLuz(){
+  
+    tiempo_transcurrido=millis()-cronometro_lecturas;
+    
+    if(tiempo_transcurrido>ESPERA_LECTURAS){// espera no bloqueante
+      
+        cronometro_lecturas=millis();
+        luminosidad=analogRead(PinLDR);
+        Serial.print("La luminosidad es del ");
+        Serial.print(luminosidad*coeficiente_porcentaje);//detectamos el porcentaje de luminosidad
+        Serial.println("%");
+    }
+
+    return luminosidad*coeficiente_porcentaje;
+  
+  }
 
 void amacarCuna(){
   if(prendoCuna == 1){
@@ -84,4 +130,21 @@ void escucharLlanto(){
   
 }
 
-
+void ruidoAmbiente(){
+ 
+    int lecturaRuido = analogRead(pinMicro);
+  
+    if(tiempoFinProm > ESPERA_LECTURAS){
+      
+      muestras += lecturaRuido;
+      Serial.print("Valor Medido: ");
+      Serial.print(lecturaRuido);
+      Serial.print("Valor Acumulado: ");
+      Serial.println(muestras);
+      
+      }
+       
+    tiempoFinProm = millis() - tiempoInicioProm;
+ 
+ 
+  }
